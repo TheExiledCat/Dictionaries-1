636190bcd754d342db728341f39ded56b541bf17
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-03-27 13:14:12 +0100

    Upload

---
+++ Dictionaries 1/Assets/DisplayAmmo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayAmmo : MonoBehaviour
+{
+    Weapon w;
+    int ammo;
+    public Text weaponType;
+    public Text ammoType;
+    WeaponSystem p;
+    
+
+   
+    void Start()
+    {
+        p = GameObject.FindGameObjectWithTag("Player").GetComponent<WeaponSystem>();
+        
+    }
+    void Update()
+    {
+        weaponType.text = "Weapon: "+p.currentWeapon.name;
+        ammoType.text = p.currentAmmoType.ToString() + ": " + p.currentWeaponAmmo;
+    }
+}
---
+++ Dictionaries 1/Assets/WeaponSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class WeaponSystem : MonoBehaviour
+{
+    
+    Dictionary<string, int> Ammo = new Dictionary<string, int>();
+    int weaponIndex;
+    [SerializeField]
+    Weapon[] weapons;
+    public Weapon currentWeapon;
+    public int currentWeaponAmmo;
+    public string currentAmmoType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Ammo.Add("Rifle", 10);
+        Ammo.Add("Rocket", 5);
+        Ammo.Add("Pistol", 20);
+        currentWeaponAmmo = Ammo[weapons[weaponIndex].ammotype];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && weaponIndex > 0)//iterator for weapon Q and E to cycle
+        {
+            weaponIndex--;
+        }else if(Input.GetKeyDown(KeyCode.Q) && weaponIndex <= 0)
+        {
+            weaponIndex = weapons.Length - 1;
+        }
+        if (Input.GetKeyDown(KeyCode.E)&&weaponIndex<weapons.Length-1)
+        {
+            weaponIndex++;
+        }else if(Input.GetKeyDown(KeyCode.E) && weaponIndex >= weapons.Length-1)
+        {
+            weaponIndex = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&&Ammo[weapons[weaponIndex].ammotype]!=0) Shoot();
+        print(weaponIndex);
+        currentWeapon = weapons[weaponIndex];
+        currentWeaponAmmo = Ammo[weapons[weaponIndex].ammotype];
+        currentAmmoType = currentWeapon.ammotype;
+    }
+   public void Shoot()
+    {
+        Ammo[weapons[weaponIndex].ammotype]--;
+        
+       
+    }
+}
